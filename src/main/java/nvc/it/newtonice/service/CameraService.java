bf22706a212fc7afdf0e7b08a559e1aea7a527cf
wnsr2602 <phutter759@gmail.com>
2022-02-22 14:27:28 +0700

    seond

--- src/main/java/nvc/it/newtonice/service/CameraService.java
+++ src/main/java/nvc/it/newtonice/service/CameraService.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Service;
 
 import nvc.it.newtonice.model.Camera;
@@ -24,17 +23,6 @@ public class CameraService {
     }
     public List<Camera> fCameras(String name){
         return cameraRepository.findByNameContaining(name);
-    }
-    public Camera adCamera(Camera camera){
-        return CameraRepository.save(camera);
-    }
-    public Optional<Product> updateProduct(String id,Product product){
-        CrudRepository<Camera, String> productRepository;
-        Product currentProduct = productRepository.findById(id).get();
-        currentProduct.setName(product.getName());
-        currentProduct.set_price(product.get_price());
-        currentProduct.setUnit_in_stock(product.getUnit_in_stock());
-        return Optional.of(productRepository.save(currentProduct));
     }
     public Optional<Camera> add(String id, Event event){
         Camera currenProduct = cameraRepository.findById(id).get();
